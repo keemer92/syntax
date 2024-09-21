00fe2fc47256f3edf1661b4b456cc1f5b5251a17
Waquiem Comar <152826683+keemer92@users.noreply.github.com>
2024-09-20 22:08:28 -0400

    Delete Syntax directory

--- Syntax/Terraform Syntax/Provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  region  = "us-west-2"        # The AWS region to create resources in
-  access_key = "YOUR_ACCESS_KEY"    # Optional: Your AWS access key
-  secret_key = "YOUR_SECRET_KEY"    # Optional: Your AWS secret key
-  # You can also use environment variables or AWS credentials file instead
-}
