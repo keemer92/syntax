c3c5f31a05e0b866c0a0dff22e983f2508ae4335
Waquiem Comar <wcomar92@gmail.com>
2024-09-23 08:29:08 -0400

    syntax for the variable.tf file

---
+++ Terraform Syntax/variables.tf
@@ -0,0 +1,33 @@
+variable "ami" {
+  description = "The AMI ID to use for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for the subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  description = "Availability zone for the subnet"
+  type        = string
+  default     = "us-east-1a"  # Adjust as needed
+}
