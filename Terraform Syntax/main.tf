623e9dd27185308e48b6aacaaf9dcf611f37470d
Waquiem Comar <wcomar92@gmail.com>
2024-09-23 08:31:02 -0400

    deleted output info

--- Terraform Syntax/main.tf
+++ Terraform Syntax/main.tf
@@ -96,20 +96,6 @@ resource "aws_iam_group" "example" {
   name = "example-group"
 }
 
-# Output the VPC ID, Subnet ID, and Security Group ID
-output "vpc_id" {
-  value = aws_vpc.my_vpc.id
-}
-
-output "subnet_id" {
-  value = aws_subnet.my_subnet.id
-}
-
-output "security_group_id" {
-  value = aws_security_group.my_security_group.id
-}
-
-
 resource "aws_iam_role" "example" {
   name               = "example_role"
   assume_role_policy = jsonencode({
