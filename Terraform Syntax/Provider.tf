012d75e224f4c9c98124bf90fb60d69e5cac44f1
Waquiem Comar <wcomar92@gmail.com>
2024-09-23 08:25:12 -0400

    syntax for the provider

--- Terraform Syntax/Provider.tf
+++ Terraform Syntax/Provider.tf
@@ -1,6 +1,4 @@
+# Specify the provider (AWS)
 provider "aws" {
-  region  = "us-west-2"        # The AWS region to create resources in
-  access_key = "YOUR_ACCESS_KEY"    # Optional: Your AWS access key
-  secret_key = "YOUR_SECRET_KEY"    # Optional: Your AWS secret key
-  # You can also use environment variables or AWS credentials file instead
+  region = "us-east-1"  # Change this to your desired region
 }
